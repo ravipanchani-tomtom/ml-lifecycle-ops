22b1be4cf426f95508702e5e5c6d615dd23ca3ad
panchani <ravi.panchani@tomtom.com>
2024-12-14 01:50:22 +0530

    feat: new model arch

--- src/model.py
+++ src/model.py
@@ -3,20 +3,25 @@ from torch.functional import F
 class MNISTModel(nn.Module):
     def __init__(self):
         super(MNISTModel, self).__init__()
-        self.conv1 = nn.Conv2d(1, 4, kernel_size=3, stride=1, padding=0)
-        self.conv2 = nn.Conv2d(4, 8, kernel_size=3, stride=1, padding=0)
-        self.fc1 = nn.Linear(8 * 7 * 7, 32)
-        self.fc2 = nn.Linear(32, 10)
+        self.conv1 = nn.Conv2d(1, 16, kernel_size=3, stride=1, padding=0)
+        self.conv2 = nn.Conv2d(16, 32, kernel_size=3, stride=1, padding=0)
+        self.global_avg_pool = nn.AdaptiveAvgPool2d((1, 1))
+        self.fc = nn.Linear(32, 10)  # Output layer
 
     def forward(self, x):
         x = F.relu(self.conv1(x))
         x = F.max_pool2d(x, 2)
         x = F.relu(self.conv2(x))
         x = F.max_pool2d(x, 2)
-        x = x.view(-1, 32 * 7 * 7)
-        x = F.relu(self.fc1(x))
-        x = self.fc2(x)
+        x = self.global_avg_pool(x)
+        x = x.view(x.size(0), -1)  # Flatten the tensor
+        x = self.fc(x)
         return x
 
     def count_parameters(self):
         return sum(p.numel() for p in self.parameters() if p.requires_grad)
+
+    def parameter_summary(self):
+        for name, param in self.named_parameters():
+            if param.requires_grad:
+                print(f'{name}: {param.numel()} parameters')
--- tests/test_train.py
+++ tests/test_train.py
@@ -2,6 +2,12 @@
 
 import unittest
 import torch
+from torch.functional import F
+from torch.optim import Adam, SGD
+
+import sys
+sys.path.append(".")
+
 from src.model import MNISTModel
 from src.train import train_model
 from src.utils import load_data
@@ -9,10 +15,11 @@ from src.utils import load_data
 class TestTrainModel(unittest.TestCase):
     def setUp(self):
         self.model = MNISTModel()
-        self.train_loader, self.test_loader = load_data()
+        self.train_loader, self.test_loader = load_data(batch_size=128)
 
     def test_training_accuracy(self):
-        accuracy = train_model(self.model, self.train_loader, num_epochs=1)
+        self.model.parameter_summary()
+        accuracy = train_model(self.model, self.train_loader,criterion=torch.nn.CrossEntropyLoss(), optimizer=Adam(self.model.parameters(), 0.001), num_epochs=1)
         self.assertGreaterEqual(accuracy, 0.95, "Model did not achieve 95% accuracy in the first epoch.")
 
 if __name__ == '__main__':
