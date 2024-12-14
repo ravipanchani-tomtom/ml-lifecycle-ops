a3239f4455201a7bde2d6246d4f97634a93c27e7
panchani <ravi.panchani@tomtom.com>
2024-12-14 14:59:23 +0530

    fix: model params with 86% accuracy

--- src/model.py
+++ src/model.py
@@ -3,19 +3,19 @@ from torch.functional import F
 class MNISTModel(nn.Module):
     def __init__(self):
         super(MNISTModel, self).__init__()
-        self.conv1 = nn.Conv2d(1, 16, kernel_size=3, stride=1, padding=0)
-        self.conv2 = nn.Conv2d(16, 32, kernel_size=3, stride=1, padding=0)
-        self.global_avg_pool = nn.AdaptiveAvgPool2d((1, 1))
-        self.fc = nn.Linear(32, 10)  # Output layer
+        self.conv1 = nn.Conv2d(1, 4, kernel_size=3, stride=1, padding=1)
+        self.conv2 = nn.Conv2d(4, 8, kernel_size=3, stride=1, padding=1)
+        self.fc1 = nn.Linear(8 * 7 * 7, 16)
+        self.fc2 = nn.Linear(16, 10)
 
     def forward(self, x):
         x = F.relu(self.conv1(x))
         x = F.max_pool2d(x, 2)
         x = F.relu(self.conv2(x))
         x = F.max_pool2d(x, 2)
-        x = self.global_avg_pool(x)
-        x = x.view(x.size(0), -1)  # Flatten the tensor
-        x = self.fc(x)
+        x = x.view(-1, 8 * 7 * 7)
+        x = F.relu(self.fc1(x))
+        x = self.fc2(x)
         return x
 
     def count_parameters(self):
--- tests/test_train.py
+++ tests/test_train.py
@@ -6,21 +6,25 @@ from torch.functional import F
 from torch.optim import Adam, SGD
 
 import sys
+
 sys.path.append(".")
 
 from src.model import MNISTModel
 from src.train import train_model
 from src.utils import load_data
 
+
 class TestTrainModel(unittest.TestCase):
     def setUp(self):
         self.model = MNISTModel()
-        self.train_loader, self.test_loader = load_data(batch_size=128)
+        self.train_loader, self.test_loader = load_data()
 
     def test_training_accuracy(self):
         self.model.parameter_summary()
-        accuracy = train_model(self.model, self.train_loader,criterion=torch.nn.CrossEntropyLoss(), optimizer=Adam(self.model.parameters(), 0.001), num_epochs=1)
+        accuracy = train_model(self.model, self.train_loader, criterion=torch.nn.CrossEntropyLoss(),
+                               optimizer=Adam(self.model.parameters(), 0.001), num_epochs=1)
         self.assertGreaterEqual(accuracy, 0.95, "Model did not achieve 95% accuracy in the first epoch.")
+
 
 if __name__ == '__main__':
     unittest.main()
